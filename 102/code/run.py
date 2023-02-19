bf98b8f2ffff02ec5f1b57503585dd5f550fc74d
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2023-02-19 14:22:26 -0500

    push demonostration

--- 102/code/run.py
+++ 102/code/run.py
@@ -1,10 +1,25 @@
-def say_hello(sep='|', name='friend'):
-    print('hello', name, sep=sep)
-    print('how are you today?')
-    print('it was nice to meet you', name, sep=sep)
+# a very minimal 'rock, paper, scissors' game
 
+from random import choice
 
-people = ['noah', 'eric', 'justin', 'jordan', 'gene']
+results = {
+    'rock': ['scissors', 'lizard'],
+    'paper': ['rock', 'spock'],
+    'scissors': ['paper', 'lizard'],
+    'lizard': ['paper', 'spock'],
+    'spock': ['rock', 'scissors']
+}
 
-for person in people:
-    say_hello('*', person)
+user_choice = input('RPSLS?')
+comp_choice = choice(list(results.keys()))
+
+print(comp_choice)
+# tie
+if user_choice == comp_choice:
+    print("it's a tie")
+# win
+elif comp_choice in results[user_choice]:
+    print('you won!')
+# lose
+else:
+    print('you lose.')
